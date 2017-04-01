daa50b46090241f46e7f756ab0fb9590da176687
Pranav Chaudhary <pranavchaudhary2310@gmail.com>
2017-04-02 03:05:59 +0530

    Improve InfoActivity's Layout

--- app/src/main/java/com/trio/sos/InfoActivity.java
+++ app/src/main/java/com/trio/sos/InfoActivity.java
@@ -9,7 +9,10 @@ import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.IdRes;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,6 +20,8 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import com.trio.sos.helper.Constants;
 import com.trio.sos.repo.LoggedUser;
@@ -34,10 +39,13 @@ public class InfoActivity extends Activity{
 
     EditText mEditName,mEditContact,mEditEmail,mEditDob;
     Button mSubmitButton;
+    RadioGroup mRadioGroup;
     Intent mData;
     CircleImageView mProfilePicture;
     ImageButton mDob ;
     Dialog mDatePickerDialog;
+    LoggedUser mUser;
+    boolean mRadioFlag = false;
 
     private class LoadImage extends AsyncTask<Uri,Void,Bitmap> {
 
@@ -67,12 +75,12 @@ public class InfoActivity extends Activity{
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
-
         mData=getIntent();
         bindActivity();
         Log.i(TAG,mData.getStringExtra(Constants.KEY_INTENT_FROM));
         if (mData.getStringExtra(Constants.KEY_INTENT_FROM).equals(LoginActivity.TAG)){
             fillDataFromIntent();
+            mUser = new LoggedUser(this);
         }else if(mData.getStringExtra(Constants.KEY_INTENT_FROM).equals(MainActivity.TAG)){
             fillDataFromLoggedUser();
         }
@@ -97,28 +105,51 @@ public class InfoActivity extends Activity{
         mEditEmail.setText(user.getEmail());
         mEditDob.setText(user.getDob());
         mEditContact.setText(user.getContactNo());
+        if (user.getGender().equals(Constants.GENDER_MALE)){
+            mRadioGroup.check(R.id.info_radio_button_male);
+        }else{
+            mRadioGroup.check(R.id.info_radio_button_female);
+        }
         mEditName.setEnabled(false);
         mEditEmail.setEnabled(false);
         mEditDob.setEnabled(false);
+        mRadioGroup.setEnabled(false);
         mEditContact.setEnabled(false);
         mSubmitButton.setVisibility(Button.GONE);
     }
 
     private void bindActivity(){
+        //binding activity to views
         mEditName= (EditText) findViewById(R.id.info_edit_name);
         mEditContact= (EditText) findViewById(R.id.info_edit_contact);
         mEditEmail= (EditText) findViewById(R.id.info_edit_email);
         mProfilePicture = (CircleImageView) findViewById(R.id.info_profilePicture);
         mDob = (ImageButton) findViewById(R.id.info_button_dob);
         mEditDob = (EditText) findViewById(R.id.info_edit_dob);
+        mSubmitButton = (Button) findViewById(R.id.info_button_submit);
+        mRadioGroup  =  (RadioGroup) findViewById(R.id.info_radio_group);
+
+
+        //Setting Listeners
+        mRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, @IdRes int checkedId) {
+                if (checkedId == R.id.info_radio_button_male){
+                    mUser.setGender(Constants.GENDER_MALE);
+                }else{
+                    mUser.setGender(Constants.GENDER_FEMALE);
+                }
+                mRadioFlag=true;
+            }
+        });
+
         mDob.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mDatePickerDialog.show();
             }
         });
-        mSubmitButton = (Button) findViewById(R.id.info_button_submit);
-        //Setting listener to submit button
+
         mSubmitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -128,7 +159,6 @@ public class InfoActivity extends Activity{
             }
         });
 
-        //Setting up the Date Picker Dialog
         DatePicker datePicker = new DatePicker(this);
         Calendar calendar = Calendar.getInstance();
         calendar.setTimeInMillis(System.currentTimeMillis());
@@ -141,6 +171,7 @@ public class InfoActivity extends Activity{
             }
         });
         mDatePickerDialog =new Dialog(this);
+        mDatePickerDialog.setTitle("Choose your Date of Birth");
         ViewGroup.LayoutParams layoutParams =new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT);
         mDatePickerDialog.addContentView(datePicker,layoutParams);
     }
@@ -150,18 +181,18 @@ public class InfoActivity extends Activity{
         if (mEditName.getText().toString().length() == 0 ){
             mEditName.setError("Name Required");
             flag=false;
-        }
-        if (mEditContact.getText().toString().length() == 0 ){
+        }else if (mEditContact.getText().toString().length() == 0 ){
             mEditContact.setError("Contact No. Required");
             flag=false;
-        }
-        if (mEditDob.getText().toString().length() == 0 ){
+        }else if (mEditDob.getText().toString().length() == 0 ){
             mEditDob.setError("Cannot be left empty");
             flag=false;
-        }
-        if (mEditEmail.getText().toString().length() == 0 ){
+        }else if (mEditEmail.getText().toString().length() == 0 ){
             mEditEmail.setError("Email Required");
             flag=false;
+        }else if (!mRadioFlag){
+            Toast.makeText(this, "Please select gender", Toast.LENGTH_SHORT).show();
+            flag = false;
         }
         return flag;
     }
@@ -173,17 +204,16 @@ public class InfoActivity extends Activity{
     }
 
     private void submit(){
-        LoggedUser user = new LoggedUser(this);
-        user.setName(mEditName.getText().toString());
-        user.setEmail(mEditEmail.getText().toString());
-        user.setContactNo(mEditContact.getText().toString());
-        user.setDob(mEditDob.getText().toString());
+        mUser.setName(mEditName.getText().toString());
+        mUser.setEmail(mEditEmail.getText().toString());
+        mUser.setContactNo(mEditContact.getText().toString());
+        mUser.setDob(mEditDob.getText().toString());
         if (mData.getParcelableExtra(Constants.KEY_PHOTO_URL) != null){
-            user.setPhotoUrl(mData.getParcelableExtra(Constants.KEY_PHOTO_URL).toString());
+            mUser.setPhotoUrl(mData.getParcelableExtra(Constants.KEY_PHOTO_URL).toString());
         }else {
-            user.setPhotoUrl(null);
+            mUser.setPhotoUrl(null);
         }
-        user.save();
+        mUser.save();
         SharedPreferences route = getSharedPreferences(
                 Constants.SHARED_PREFERENCE_ROUTE,MODE_PRIVATE);
         SharedPreferences.Editor edit = route.edit();
